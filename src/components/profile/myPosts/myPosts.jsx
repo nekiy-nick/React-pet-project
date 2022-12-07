52faeb5b8a8d2ca11d390a3e0bf58f014b6a7295
KhitrovNickolai <deliriumbred@yandex.ru>
2022-12-07 18:44:08 +0300

    Add submodules profile. Create post and myPosts components

---
+++ src/components/profile/myPosts/myPosts.jsx
@@ -0,0 +1,15 @@
+import Post from'.././post/post.jsx';
+
+const MyPosts = () => {
+	return (
+		<div>
+			My
+			<Post />
+			<Post />
+			<Post />
+
+		</div>
+	)
+}
+
+export default MyPosts;
---
+++ src/components/profile/post/post.jsx
@@ -0,0 +1,15 @@
+import s from './post.module.css';
+
+const Post = () => {
+	return (
+		<div className={s.post}>
+			<img src="" />
+
+			<div className={s.description}>
+				<h3 className={s.title}>Title of a post</h3>
+			</div>
+		</div>
+	);
+};
+
+export default Post;
